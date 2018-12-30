24176235302aafbafa641494e9b19c4f66836d48
Vladimir Zverev <wow4ik1997@mail.ru>
2018-12-30 14:37:54 +0300

    Uprate AppDelegate.swift

--- Gitdemo/AppDelegate.swift
+++ Gitdemo/AppDelegate.swift
@@ -15,7 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        for i in 1...3 {
+            print("\(i)")
+        }
         return true
     }
 
