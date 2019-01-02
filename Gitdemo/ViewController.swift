1bf43211810067656a54debd3f6ac2e04a7e94d8
Vladimir Zverev <wow4ik1997@mail.ru>
2019-01-02 20:21:19 +0300

    Revert "ViewController update"
    This reverts commit 2d7008df380661710a9acbfaa78a0421ee2b95a8.

---
+++ Gitdemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Gitdemo
+//
+//  Created by Володя Зверев on 30.12.2018.
+//  Copyright © 2018 Володя Зверев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello World!")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
